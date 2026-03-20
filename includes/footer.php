adef891014fcb8463b48346489100a750757b497
shresthaarun297-pixel <shresthaarun297@gmail.com>
2026-03-20 08:05:03 +0100

    Separated helper function and shared header footer files correctly

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    <footer class="site-footer">
+        <div class="site-footer__inner">
+            <p>&copy; <?= date('Y') ?> Student Hub. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
