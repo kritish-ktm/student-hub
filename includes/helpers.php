0cdcfb2ec86f9ba4559da7a99fcf08ecf6d18130
shresthaarun297-pixel <shresthaarun297@gmail.com>
2026-03-20 08:23:05 +0100

    Added shared redirect and integer helper functions

--- includes/helpers.php
+++ includes/helpers.php
@@ -2,3 +2,12 @@
 function e($value) {
     return htmlspecialchars((string)$value, ENT_QUOTES, 'UTF-8');
 }
+
+function redirect($path) {
+    header('Location: ' . $path);
+    exit;
+}
+
+function getInt($value, $default = 0) {
+    return filter_var($value, FILTER_VALIDATE_INT) !== false ? (int)$value : $default;
+}
